59b2f670baa970df95545e6340b5b39b8a29f5d1
Tom Verbeure <hombre+github@gmail.com>
2019-04-13 12:18:29 -0700

    Add seed...

--- quartus/run_quartus.sh
+++ quartus/run_quartus.sh
@@ -1,6 +1,6 @@
 rm -fr ./db
 rm -fr ./incremental_db
 $QUARTUS_ROOTDIR/bin/quartus_map --read_settings_files=on --write_settings_files=off VexRiscv -c VexRiscv
-$QUARTUS_ROOTDIR/bin/quartus_fit --read_settings_files=off --write_settings_files=off VexRiscv -c VexRiscv
+$QUARTUS_ROOTDIR/bin/quartus_fit --seed=2 --read_settings_files=off --write_settings_files=off VexRiscv -c VexRiscv
 $QUARTUS_ROOTDIR/bin/quartus_sta --model=slow VexRiscv -c VexRiscv
 
